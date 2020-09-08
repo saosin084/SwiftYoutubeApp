56ca52e8af3267d86030162e890516520a48b61b
Yuta <yuta5196@gmail.com>
2020-09-08 18:08:34 +0900

    Initial Commit

---
+++ SwiftYoutuberApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftYoutuberApp1
+//
+//  Created by yuta on 2020/09/08.
+//  Copyright © 2020 yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

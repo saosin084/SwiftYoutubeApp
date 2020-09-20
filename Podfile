7074abf6e6aa9caf59d6203226f36ea9621e9057
Yuta <yuta5196@gmail.com>
2020-09-20 18:08:55 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SwiftYoutuberApp1' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SwiftYoutuberApp1
+
+pod 'Alamofire', '~> 5.0.0-beta.5'
+pod 'SwiftyJSON', '~> 4.0'
+pod 'SegementSlide'
+pod 'SDWebImage', '~> 5.0'
+ 
+end
---
+++ SwiftYoutuberApp1/Model/YoutubeData.swift
@@ -0,0 +1,19 @@
+//
+//  YoutubeData.swift
+//  SwiftYoutuberApp1
+//
+//  Created by yuta on 2020/09/11.
+//  Copyright © 2020 yuta. All rights reserved.
+//
+
+import Foundation
+
+class YoutubeData{
+    
+    var videoId:String = ""
+    var publishedAt:String = ""
+    var title:String = ""
+    var imageURLString:String = ""
+    var youtubeURL:String = ""
+    var channelTitle:String = ""
+}
